574b353e460d33b91a212d4032344f30dbf82c9f
laluna93 <91890324+laluna93@users.noreply.github.com>
2022-01-24 13:20:05 +0400

    feat: update file

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict'
+<<<<<<< HEAD
+=======
+
+>>>>>>> 88848b4a3d25983ea86cbceec6d8a21b8d478c40
+const burg = document.querySelector('.burg')
+burg.addEventListener('click', () => { 
+    burg.classList.toggle("active");
+    console.log("a");
+})
+const navMenu = document.querySelector('.menu-nav')
+<<<<<<< HEAD
+navMenu.addEventListener('click', () => { 
+    navMenu.classList.toggle("active");
+=======
+
+>>>>>>> 88848b4a3d25983ea86cbceec6d8a21b8d478c40
+    console.log("1");
+})
+
+
