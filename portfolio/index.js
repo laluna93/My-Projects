86859d42cfa7a8e509456e27289d6db51e883f7b
Anna <Laluna-93@yandex.ru>
2022-01-24 12:28:44 +0400

    refactor: changes have been made to the structure of the code

--- portfolio/index.js
+++ portfolio/index.js
@@ -8,17 +8,23 @@
 // boxMenu.addEventListener('click', () => {
 //     boxMenu.classList.toggle("active");
 // });
-// (function() {
-// const menuBurg = document.querySelector('.menu-nav');
-// menuBurg.addEventListener('click' , () => {
-//     console.log('123');
-// });
-// }());
+(function() {
+const menuBurg = document.querySelector('.burg');
+menuBurg.addEventListener('click' , () => {
+    console.log('123');
+});
+}());
 
 
-const burg = document.querySelector('.span-burg')
-burg.addEventListener('click', () => { 
-    burg.classList.toggle("active");
-    console.log("1");
+// const burg = document.querySelector('.burg')
+// burg.addEventListener('click', () => { 
+//     burg.classList.toggle("active");
+//     console.log("1");
     
-})
+// })
+
+// const hamburger = document.querySelector('.burg');
+
+//     hamburger.addEventListener('click', () => {
+//         hamburger.classList.toggle('active');
+//     });
