cfa5575459278778c9b99dc95255424a50a5d0c8
laluna93 <91890324+laluna93@users.noreply.github.com>
2022-01-24 11:37:50 +0400

    refactor: changes have been made to the structure of the code

--- portfolio/index.js
+++ portfolio/index.js
@@ -2,8 +2,17 @@
 const burg = document.querySelector('.burg')
 burg.addEventListener('click', () => { 
     burg.classList.toggle("active");
+<<<<<<< portfolio-part2
     console.log("1");
     
+=======
+    console.log("a");
+})
+const navMenu = document.querySelector('.menu-nav')
+burg.addEventListener('click', () => { 
+    burg.classList.toggle("active");
+    console.log("1");
+>>>>>>> portfolio
 })
 
 const menu = document.querySelector('.menu-nav')
